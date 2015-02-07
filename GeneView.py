fa322b92fea5a3259ae348fdc2895c3c9fe5c947
Alexander Niema Moshiri <niemamoshiri@gmail.com>
2015-02-07 14:09:39 -0800

    GeneView initial completion

--- GeneView.py
+++ GeneView.py
@@ -1,20 +1,14 @@
-# Takes VCF file and returns dictionary. Keys = Gene Names; Values = list of <mutation_name>@<number(freq?)>
-def parseVCF( file ):
-    first = True
+# parse text file (contains gene name + mutation)
+def parseMutationList( file ):
     out = {}
     for line in file:
-        # ignore first line
-        if first:
-            first = False
+        parts = line.strip().split()
+        geneID = parts[0]
+        mutation = parts[1]
+        if geneID in out:
+            out[geneID].append(mutation)
         else:
-            stripped = line.strip()
-            #do stuff to actually find gene ID and mutation
-            mutationID = "TEST_MUT@100" #change 100 to actual frequency
-            geneID = "TEST_GENE"
-            if geneID in out:
-                out[geneID].append(mutationID)
-            else:
-                out[geneID] = [mutationID]
+            out[geneID] = [mutation]
     return out
 
 import sys
@@ -24,7 +18,7 @@ if __name__ == "__main__":
         print "ERROR: Incorrect number of arguments. Correct usage:"
         print "GeneView.py <vcf_file>"
         sys.exit()
-    genesAndMutations = parseVCF(open(sys.argv[1],'r')) # parse VCF and return dict, where dict[geneName] = [list of mutationName@freq]
+    genesAndMutations = parseMutationList(open(sys.argv[1],'r')) # parse VCF and return dict, where dict[geneName] = [list of mutationName@freq]
     if not os.path.exists("images"): # create images folder if it doesn't exist
         os.makedirs("images")
     for gene in genesAndMutations: # for every gene:
