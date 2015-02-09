e0143a46662c8e9a2b11034b214e547c4bbe954a
Alexander Niema Moshiri <niemamoshiri@gmail.com>
2015-02-08 19:05:47 -0800

    Fixed directories and added index.html

--- GeneView.py
+++ GeneView.py
@@ -11,17 +11,17 @@ def parseMutationList( file ):
             out[geneID] = [mutation]
     return out
 
-def generateLollipop( gene ):
+def generateLollipop( gene, genes ):
     command = ["./lollipops","-labels"]
-    command.append("-o=images/"+gene+".svg")
+    command.append("-o="+outFolder"/images/"+gene+".svg")
     command.append(gene)
-    for mutation in genesAndMutations[gene]:
+    for mutation in genes[gene]:
         edit = mutation.replace("\xe2\x80\x8f","")
         command.append(edit)
     call(command)
 
 def generateHTML( gene ):
-    html = open("pages/"+gene+".html",'w')
+    html = open(outFolder + "/pages/"+gene+".html",'w')
     html.write('<html>')
     html.write('  <head>')
     html.write('    <title>' + gene + '</title>')
@@ -37,6 +37,15 @@ def generateHTML( gene ):
     html.write('  </body>')
     html.write('</html>')
 
+def generateIndexHTML( genes ):
+    html = open(outFolder + "/index.html",'w')
+    html.write('<html>')
+    html.write('  <head>')
+    html.write('    <title>GeneView</title>')
+    html.write('    <link rel="stylesheet" type="text/css" href="GeneView_style.css">')
+    html.write('  </head>')
+    html.write('</html>')
+
 import sys
 import os
 from subprocess import call
@@ -45,12 +54,17 @@ if __name__ == "__main__":
         print "ERROR: Incorrect number of arguments. Correct usage:"
         print "GeneView.py <vcf_file>"
         sys.exit()
+    outFolder = "output"
     genesAndMutations = parseMutationList(open(sys.argv[1],'r')) # parse VCF and return dict, where dict[geneName] = [list of mutationName@freq]
-    if not os.path.exists("images"): # create images folder if it doesn't exist
-        os.makedirs("images")
-    if not os.path.exists("pages"): # create pages folder if it doesn't exist
-        os.makedirs("pages")
+    if not os.path.exists(outFolder):
+        os.makedirs(outFolder)
+        os.makedirs(outFolder + "/images")
+        os.makedirs(outFolder +"/pages")
+    if not os.path.exists(outFolder + "/images"): # create images folder if it doesn't exist
+        os.makedirs(outFolder + "/images")
+    if not os.path.exists(outFolder + "/pages"): # create pages folder if it doesn't exist
+        os.makedirs(outFolder + "/pages")
     for gene in genesAndMutations: # generate a lollipop image and HTML page for each gene
-        generateLollipop(gene)
+        generateLollipop(gene,genesAndMutations)
         generateHTML(gene)
-    #generate index
+    generateIndexHTML(genesAndMutations) # generate index.html
