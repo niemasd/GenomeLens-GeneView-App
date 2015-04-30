1c5ace2cff27b63af16005773f7370c9bc65cadd
Alexander Niema Moshiri <niemamoshiri@gmail.com>
2015-04-30 13:39:26 -0700

    Create ParseDAT.py

---
+++ scripts/ParseDAT.py
@@ -0,0 +1,34 @@
+# Pass in a "file" object, not a filename string
+# i.e. readTable(open('HUMAN_9606_idmapping.dat'))
+# The returned dictionary has the following structure:
+#   KEYS:   ENSEMBL ID or REFSEQ ID
+#   VALUES: Uniprot ID
+
+
+def parseDAT(f):
+    genes = {} # genes[gene] = [RefSeq,Ensembl_TRS,UniProtKB-AC]
+    for line in f:
+        if 'RefSeq\t' in line:
+            parts = line.strip().split('\t')
+            if parts[0] not in genes:
+                genes[parts[0]] = ['','','']
+            genes[parts[0]][0] = parts[2]
+        elif 'Ensembl_TRS\t' in line:
+            parts = line.strip().split('\t')
+            if parts[0] not in genes:
+                genes[parts[0]] = ['','','']
+            genes[parts[0]][1] = parts[2]
+        elif 'UniProtKB-AC\t' in line:
+            parts = line.strip().split('\t')
+            if parts[0] not in genes:
+                genes[parts[0]] = ['','','']
+            genes[parts[0]][2] = parts[2]
+    dic = {}
+    for gene in genes:
+        parts = genes[gene]
+        dic[parts[1]] = parts[2]
+        dic[parts[0]] = parts[2]
+    return dic
+
+d = parseDAT(open('db/HUMAN_9606_idmapping.dat'))
+print len(d)
