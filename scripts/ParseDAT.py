a92e169a67792585bd75b92b0cd07a076848e168
Alexander Niema Moshiri <niemamoshiri@gmail.com>
2015-05-13 17:53:58 -0700

    Output Tuple of (UniProt ID, Gene Name) instead of just the UniProt ID

--- scripts/ParseDAT.py
+++ scripts/ParseDAT.py
@@ -2,36 +2,41 @@
 # i.e. readTable(open('HUMAN_9606_idmapping.dat'))
 # The returned dictionary has the following structure:
 #   KEYS:   ENSEMBL ID or REFSEQ ID
-#   VALUES: Uniprot ID
+#   VALUES: Tuple of (Uniprot ID, Gene Name)
 
 
 def parseDAT(f):
-    genes = {} # genes[gene] = [RefSeq,Ensembl_TRS,UniProtKB-AC]
+    genes = {} # genes[key] = [RefSeq,Ensembl_TRS,UniProtKB-AC,gene_name]
     for line in f:
         if 'RefSeq' in line:
             parts = line.strip().split('\t')
             if parts[0] not in genes:
-                genes[parts[0]] = [[],[],'']
+                genes[parts[0]] = [[],[],'','']
             genes[parts[0]][0].append(parts[2])
+        elif 'ChiTaRS\t' in line or 'GeneWiki\t' in line:
+            parts = line.strip().split('\t')
+            if parts[0] not in genes:
+                genes[parts[0]] = [[],[],'','']
+            genes[parts[0]][3] = parts[2]
         elif 'Ensembl_TRS\t' in line:
             parts = line.strip().split('\t')
             if parts[0] not in genes:
-                genes[parts[0]] = [[],[],'']
+                genes[parts[0]] = [[],[],'','']
             genes[parts[0]][1].append(parts[2])
         #elif 'UniProtKB-AC\t' in line:
         elif 'UniProtKB-ID\t' in line:
             parts = line.strip().split('\t')
             if parts[0] not in genes:
-                genes[parts[0]] = [[],[],'']
+                genes[parts[0]] = [[],[],'','']
             genes[parts[0]][2] = parts[2]
             #print "UNIPROT: " + parts[2]
     dic = {}
-    for gene in genes:
-        refseq,ensembl,uniprot = genes[gene]
+    for key in genes:
+        refseq,ensembl,uniprot,genename = genes[key]
         for r in refseq:
-            dic[r] = uniprot
+            dic[r] = (uniprot,genename)
         for e in ensembl:
-            dic[e] = uniprot
+            dic[e] = (uniprot,genename)
         #dic[parts[1]] = parts[2]
         #dic[parts[0]] = parts[2]
     return dic
